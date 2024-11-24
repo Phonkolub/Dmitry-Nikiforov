df139b993ca79a79c5d8abba8a6a2200257e1b1b
Phonkolub <bercerkhots@gmail.com>
2024-11-24 14:21:28 +0300

    s

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"os"
 	"strings"
 
@@ -12,7 +11,7 @@ import (
 func main() {
 	text := &text.Text{}
 	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("Hello")
+
 	for scanner.Scan() {
 		text.Content = scanner.Text()
 
